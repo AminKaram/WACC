17b3b3704f269f5b62be2718b6e7c0388b603568
Daniel  Grumberg <daniel.grumberg14@imperial.ac.uk>
2015-11-15 10:51:13 +0000

    Started work on visit functions.

--- src/ast-node-visitor.cc
+++ src/ast-node-visitor.cc
@@ -1,18 +1,40 @@
 #include "ast-node-visitor.hh"
 
-SemanticId*
-AstNodeVisitor::lookUp(std::string& id) {
-  std::map<std::string, SemanticId>::iterator it = symbolTables.find(id);
-  if(it != symbolTables.end()) {
-     return &(symbolTable[id]);
-  } 
-  return NULL;
+void AstNodeVisitor::visit(Program *node) {
+   scope.add("int", IntType(NULL));
+   scope.add("char", CharType(NULL));
+   scope.add("string", StringType(NULL));
+   scope.add("bool", BoolType(NULL));
+   scope.add("array", Array(NULL)); 
+   scope = SymbolTable(scope);
+   node->functions.accept(this);
+   node->statements.accept(this);
 }
 
-SemanticId*
-AstNodeVisitor::lookUpAll(std::string& id) {
-  for(auto it = symbolTables.begin(); it != symbolTables.end(); it++) {
-    SemanticId *obj =  
+void AstNodeVisitor::visit(StatSeq *node) {
+  for(int i = 0; i < node->statements.size(); i++) {
+    (node->statements)[i]->accept(this)
   }
 }
 
+void AstNodeVisitor::visit(FuncDecList *node) {
+  for(int i = 0; i < node->statements.size(); i++) {
+    (node->funcs)[i]->accept(this)
+  }
+}
+
+void AstnodeVisitor::visit(FunctionDeclaration *node) {
+  std::vector<Param> params;
+  for(int i = 0; i < node->parameters->size(); i++) {
+    params.push_back(Param((*(node->parameters))[i],
+                              lookUpAll((*(node->parameters))[i]->type->name)));
+  }
+  function tmp(*node, scope.lookUpAll(node->type->name), params);
+  scope.add(node->id->id, tmp);
+  scope = SymbolTable(scope);
+  scope.add("", tmp.returnType);
+  for (int i = 0; i < node->parameters->size(); i++) {
+    ((node->parameters)[i])->accept(this);
+  }
+  node->block->accept(this);
+}
