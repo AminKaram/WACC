b77bdea8853d4b172aaee4ea558bf3ecd1e89afb
mak514 <mak514@ic.ac.uk>
2015-12-03 11:25:55 +0000

    added skeleton for code gen visitor

---
+++ src/code-gen-visitor.cc
@@ -0,0 +1,79 @@
+#ifndef CODE_GEN_VISITOR_HH
+#define CODE_GEN_VISITOR_HH
+#include "semantic-id.hh"
+#include "astnode.hh"
+#include "symbol-table.hh"
+
+class CodeGenVisitor {
+public:
+  CodeGenVisitor();
+  ~CodeGenVisitor();
+
+  void defineLabel(String label);
+
+  void visit(ASTnode *node);
+  void visit(Program *node);
+  void visit(AssignRhs *node);
+  void visit(AssignLhs *node);
+  void visit(Expression *node);
+  void visit(StatSeq *node);
+  void visit(FunctionDecList *node);
+  void visit(IntegerType *node);
+  void visit(BoolType *node);
+  void visit(CharType *node);
+  void visit(StringType *node);
+  void visit(ArrayType *node);
+  void visit(PairType *node);
+  void visit(VariableDeclaration *node);
+  void visit(FunctionDeclaration *node);
+  void visit(FunctionCall *node);
+  void visit(Assignment *node);
+  void visit(FreeStatement *node);
+  void visit(ReturnStatement *node);
+  void visit(ExitStatement *node);
+  void visit(BeginStatement *node);
+  void visit(IfStatement *node);
+  void visit(WhileStatement *node);
+  void visit(ReadStatement *node);
+  void visit(PrintStatement *node);
+  void visit(PrintlnStatement *node);
+  void visit(Number *node);
+  void visit(Boolean *node);
+  void visit(Char *node);
+  void visit(String *node);
+  void visit(Null *node);
+  void visit(BinaryOperator *node);
+  void visit(Identifier *node);
+  void visit(ArrayElem *node);
+  void visit(PairElem *node);
+  void visit(ArrayLiter *node);
+  void visit(NewPair *node);
+  void visit(UnaryOperator *node);
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // !CODE_GEN_VISITOR_HH
