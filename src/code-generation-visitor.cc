1ef8f244a610d5474830c26803fd6b0f4e3ced83
Mihai  Barbu <mihai.barbu14@imperial.ac.uk>
2015-12-08 14:54:48 +0000

    wierd test error in IOLoop

--- src/code-generation-visitor.cc
+++ src/code-generation-visitor.cc
@@ -323,27 +323,27 @@ void CodeGenVisitor::printMsgRead(TypeId *type) {
   if (charTypeId) {
     middle <<
         "  BL p_read_char" << "\n";
-      if (!msgChar) {
-        msgChar = true;
+//      if (!msgChar) {
+ //       msgChar = true;
         begin << 
            "msg_" << messageNum << ":" << std::endl <<
            "  .word 4" << std::endl <<
            "  .ascii  \"%c\\0\"" << std::endl;
         charMessageNum = messageNum;
         messageNum++;
-      }
+//      }
   } else if(intTypeId) {
       middle <<
           "  BL p_read_int" << "\n";
-      if (!msgInt){
-        msgInt = true;
+//      if (!msgInt){
+//        msgInt = true;
         begin << 
            "msg_" << messageNum << ":" << std::endl <<
            "  .word 3" << std::endl <<
            "  .ascii  \"%d\\0\"" << std::endl;
         intMessageNum = messageNum;
         messageNum++;
-      }
+ //     }
   }
 }
 
