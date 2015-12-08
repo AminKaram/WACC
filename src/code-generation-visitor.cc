981544758aea9180d8fc11fe942927cb43183742
Daniel  Grumberg <daniel.grumberg14@imperial.ac.uk>
2015-12-08 01:32:26 +0000

    manyVariables works locally. DG

--- src/code-generation-visitor.cc
+++ src/code-generation-visitor.cc
@@ -23,15 +23,19 @@ void CodeGenVisitor::visit(Program *node) {
   middle << "main:"       << "\n"
          << "  PUSH {lr}\n";
   int tmp = scopeSize;
+  std::string toEnd("");
   while (tmp > 1024) {
-    middle << "  SUB sp, sp, #" << tmp <<"\n";
+    middle << "  SUB sp, sp, #1024\n";
+    toEnd += "  ADD sp, sp, #1024\n";
     tmp -=1024;
   }
         
   middle << "  SUB sp, sp, #" << tmp << "\n";
+  toEnd +=  "  ADD sp, sp, #" + std::to_string(tmp) + "\n";
 
   node->statements->accept(this);
-  middle << "  LDR R0, =0" << "\n"
+  middle << toEnd
+         << "  LDR R0, =0" << "\n"
          << "  POP {pc}" << "\n"
          << "  .ltorg"   << "\n";
 
