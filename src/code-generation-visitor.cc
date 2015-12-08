e729a95b55e8894a2660c8018d7ede7d6bca79ce
Mihai Barbu <mihai.barbu14@imperial.ac.uk>
2015-12-08 03:54:12 +0000

    i think read works

--- src/code-generation-visitor.cc
+++ src/code-generation-visitor.cc
@@ -261,29 +261,28 @@ void CodeGenVisitor::visit(WhileStatement *node) {
 }
 
 void CodeGenVisitor::printAssemblyOfReadInt() {
-  middle << 
-        "p_read_int:" << "\n" <<
-        "  PUSH {lr}" << "\n" <<
-        "  MOV r1, r0" << "\n" <<
-        "  LDR r0, =msg_"<< intMessageNum << "\n" <<
-        "  ADD r0, r0, #4" << "\n" <<
-        "  BL scanf" << "\n" <<
-        "  POP {pc}" << "\n";
+  end << 
+      "p_read_int:" << "\n" <<
+      "  PUSH {lr}" << "\n" <<
+      "  MOV r1, r0" << "\n" <<
+      "  LDR r0, =msg_"<< intMessageNum << "\n" <<
+      "  ADD r0, r0, #4" << "\n" <<
+      "  BL scanf" << "\n" <<
+      "  POP {pc}" << "\n";
 }
 
 void CodeGenVisitor::printAssemblyOfReadChar() {
-  middle << 
-        "p_read_char:" << "\n" <<
-        "  PUSH {lr}" << "\n" <<
-        "  MOV r1, r0" << "\n" <<
-        "  LDR r0, =msg_"<< charMessageNum << "\n" <<
-        "  ADD r0, r0, #4" << "\n" <<
-        "  BL scanf" << "\n" <<
-        "  POP {pc}" << "\n";
+  end << 
+      "p_read_char:" << "\n" <<
+      "  PUSH {lr}" << "\n" <<
+      "  MOV r1, r0" << "\n" <<
+      "  LDR r0, =msg_"<< charMessageNum << "\n" <<
+      "  ADD r0, r0, #4" << "\n" <<
+      "  BL scanf" << "\n" <<
+      "  POP {pc}" << "\n";
 }
 
 void CodeGenVisitor::printMsgRead(TypeId *type) {
-  std::cout << "printMsgRead" << std:: endl;
   IntTypeId *intTypeId   = dynamic_cast<IntTypeId*> (type);
   CharTypeId *charTypeId = dynamic_cast<CharTypeId*> (type);
   
@@ -320,29 +319,20 @@ void CodeGenVisitor::printMsgRead(TypeId *type) {
 }
 
 void CodeGenVisitor::printStatementForRead(TypeId *type) {
-  std::cout << "printStatementForRead" << std:: endl;
   if (!p_read_char && type->equals(new CharTypeId())) {
       p_read_char = true;
-      std::cout << "printStatementForReadif" << std:: endl;
       printAssemblyOfReadChar();
   } else if (!p_read_int && type->equals(new IntTypeId())) {
       p_read_int = true;
-      std::cout << "printStatementForReadelse" << std:: endl;
       printAssemblyOfReadInt();
   }
 }
 
 void CodeGenVisitor::visit(ReadStatement *node) {
-  //std::cout << "visit" << std:: endl;
   node->id->accept(this);
-  //std::cout << "visit" << std:: endl;
   TypeId *type = node->id->type;
-  if (!type) 
-  std::cout << "visit" << std:: endl;
   printMsgRead(type);
-  //std::cout << "visit" << std:: endl;
   printStatementForRead(type);
-  //std::cout << "visit" << std:: endl;
 }
 
 
@@ -449,7 +439,6 @@ void CodeGenVisitor::printAssemblyOfPrintBool() {
 		"  MOV r0, #0" << std::endl<<
 		"  BL fflush" << std::endl<<
 		"  POP {pc}" << "\n";
-	   std::cout << boolMessageNum << std::endl;
 }
 
 void CodeGenVisitor::printAssemblyOfPrintInt() {
@@ -518,7 +507,6 @@ void CodeGenVisitor::visit(PrintlnStatement *node) {
 void CodeGenVisitor::visit(SkipStatement *node) { }
 
 void CodeGenVisitor::visit(Number *node, std::string reg) {
-  std::cout << node->value << std::endl;
   middle << "  LDR " << reg << ", =" << node->value << std::endl;
 }
 void CodeGenVisitor::visit(Boolean *node, std::string reg) {
